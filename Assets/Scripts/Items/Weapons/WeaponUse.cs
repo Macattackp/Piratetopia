ed88d99a4294868317d0361d56b8723ceb4d623f
Macattackp <mal_t@rogers.com>
2018-03-15 22:53:27 -0400

    Starting to lay the groundwork for health and weapons. Weapon switching enabled

---
+++ Assets/Scripts/Items/Weapons/WeaponUse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    Unarmed,
+    Melee,
+    Ranged
+}
+
+public class WeaponUse : MonoBehaviour {
+
+    public WeaponType weaponType = WeaponType.Unarmed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void WeaponSwitch()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            weaponType = WeaponType.Unarmed;
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            weaponType = WeaponType.Melee;
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            weaponType = WeaponType.Ranged;
+        }
+    }
+}

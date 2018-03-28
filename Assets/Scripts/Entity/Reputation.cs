3b8d356d4981f91e0e25b79826347978028e85c5
Macattackp <mal_t@rogers.com>
2018-03-28 00:28:52 -0400

    Created "Reputation Tracker 1.0" Basic systems give the base functionality of the reputation system without applying it to NPC's nor having it affect AI

---
+++ Assets/Scripts/Entity/Reputation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// TODO:
+/// 1. Create way to add/remove reputation based on call to love, like, dislike, hate (love +2, like +1, dislike -1, hate -2
+/// 2. Create way to randomly assign love/like/dislike/hate where if a is one be is opposite
+/// 3. Connect to call to entity properties
+/// 4. Make way to call reputation check when meeting new players (creating new reputation for each entity and organization met)
+/// 5. Make way to easily call reputation check when actions or events take place
+/// 6. Tune with changing reputation points based on entity's current reputation (more hate/less love for entity that is hated and vice versa)
+/// </summary>
+
+
+public class Reputation : MonoBehaviour {
+
+    public string choiceA;
+    public string choiceB;
+	
+}
---
+++ Assets/Scripts/Entity/ReputationTracker.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReputationTracker : MonoBehaviour {
+
+    public int maxLovesHates = 3;
+    public int maxLikesDislikes = 6;
+
+    //******************POTENTIALLY TEMPORARY UNTIL NPC CREATION ESTABLISHED***************************
+
+    //HashSets to avoid duplicates
+    public HashSet<string> loveList = new HashSet<string>();
+    public HashSet<string> hateList = new HashSet<string>();
+    public HashSet<string> likeList = new HashSet<string>();
+    public HashSet<string> dislikeList = new HashSet<string>();
+
+    public int reputation = 0;
+    public int loveHateModifier = 3;
+    public int likeDislikeModifier = 1;
+    
+
+    //************************************************************************************************
+
+    public List<Reputation> reputationList = new List<Reputation>();
+
+	// Use this for initialization
+	void Start () {
+        ListDeclaration();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void reputationCheck(string call)
+    {
+        
+        if (loveList.Contains(call))
+        {
+            reputation += loveHateModifier;
+        }
+
+        if (likeList.Contains(call))
+        {
+            reputation += likeDislikeModifier;
+        }
+
+        if (dislikeList.Contains(call))
+        {
+            reputation -= likeDislikeModifier;
+        }
+
+        if (hateList.Contains(call))
+        {
+            reputation -= loveHateModifier;
+        }
+    }
+
+    void reputationAssignment()
+    {
+        int i = 0;
+        int listSelection = 0;
+        int coinToss = 0;
+        string a;
+        string b;
+        while (i < maxLovesHates)
+        {
+            listSelection = Random.Range(0,reputationList.Count-1);
+            coinToss = Random.Range(1, 2);
+
+            if (coinToss == 1)
+            {
+                a = reputationList[listSelection].choiceA;
+                b = reputationList[listSelection].choiceB;
+            }
+            else
+            {
+                a = reputationList[listSelection].choiceB;
+                b = reputationList[listSelection].choiceA;
+            }
+
+            loveList.Add(a);
+            hateList.Add(b);
+
+            i++;
+        }
+
+        while (i < maxLikesDislikes)
+        {
+            listSelection = Random.Range(0, reputationList.Count - 1);
+            coinToss = Random.Range(1, 2);
+
+            if (coinToss == 1)
+            {
+                a = reputationList[listSelection].choiceA;
+                b = reputationList[listSelection].choiceB;
+            }
+            else
+            {
+                a = reputationList[listSelection].choiceB;
+                b = reputationList[listSelection].choiceA;
+            }
+
+            likeList.Add(a);
+            dislikeList.Add(b);
+
+            i++;
+        }
+    }
+
+    void ListDeclaration()
+    {
+        ListAdd("Courage", "Cowardice");                //1
+        ListAdd("Benevolance", "Greed");                //2
+        ListAdd("Proactive", "Lazy");                   //3
+        ListAdd("Navy", "Pirate");                      //4
+        ListAdd("Treasure", "Upgrades");                //5
+        ListAdd("Melee", "Range");                      //6
+        ListAdd("Stealth", "Loud");                     //7
+        ListAdd("Gourmet", "Cheap Food");               //8
+        ListAdd("Allies", "Loner");                     //9
+        ListAdd("Loyalty", "Free Spirit");              //10
+        ListAdd("Good Sailing", "Good Fighting");       //11
+        ListAdd("Vengeance", "Mercy");                  //12
+        ListAdd("Monster Hunting", "Bounty Hunting");   //13
+        ListAdd("Cheese", "Onions");                    //14
+    }
+
+    void ListAdd(string a, string b)
+    {
+        reputationList.Add(new Reputation
+        {
+            choiceA = a,
+            choiceB = b
+        });
+    }
+}
